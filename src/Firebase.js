1919605096ed10f5586eb320d7e585d1d668e9e6
Adnan khan <khanadnan3876@gmail.com>
2024-06-18 18:40:42 +0530

    add .env

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsM_y6s_VMMOlY_lkhzbYhMpxC8TrJJZc",
-  authDomain: "disney-plus-bafa7.firebaseapp.com",
-  projectId: "disney-plus-bafa7",
-  storageBucket: "disney-plus-bafa7.appspot.com",
-  messagingSenderId: "749259967210",
-  appId: "1:749259967210:web:8c3007bd394c3a37346593"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
